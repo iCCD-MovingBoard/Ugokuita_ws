260d9a56d7b45d0a24c24293bbf2675392fb72c4
acaValkyrie <acavalkyrie@gmail.com>
2023-12-14 21:15:48 +0900

    add motor topic

--- ros2_ugokuita_ws/src/controller_pkg/setup.py
+++ ros2_ugokuita_ws/src/controller_pkg/setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 package_name = 'controller_pkg'
+submodules = 'controller_pkg/lib'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name, submodules],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
