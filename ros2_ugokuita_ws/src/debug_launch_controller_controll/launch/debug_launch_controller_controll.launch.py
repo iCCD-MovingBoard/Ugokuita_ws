63260b00cc022a3aa72b6bb6abe746e2143434b2
Valkyrie <60992540+acaValkyrie@users.noreply.github.com>
2023-12-21 21:07:25 +0900

    debug_launch_controller_controll.launch.py の更新

--- ros2_ugokuita_ws/src/debug_launch_controller_controll/launch/debug_launch_controller_controll.launch.py
+++ ros2_ugokuita_ws/src/debug_launch_controller_controll/launch/debug_launch_controller_controll.launch.py
@@ -4,7 +4,7 @@ import launch.substitutions
 import launch_ros.actions
 
 def generate_launch_description():
-  return launch.LaunchDescription()([
+  return launch.LaunchDescription([
     launch_ros.actions.Node(
       package='controller_pkg', executable='controller_publisher', output='screen'
     ),
