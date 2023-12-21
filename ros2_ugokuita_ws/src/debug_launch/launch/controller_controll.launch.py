740e6c9e6d99997f6da93bb475facd6bd0ed3e88
acaValkyrie <acavalkyrie@gmail.com>
2023-12-21 22:10:11 +0900

    Update controller_controll.launch.py to support different OS versions

--- ros2_ugokuita_ws/src/debug_launch/launch/controller_controll.launch.py
+++ ros2_ugokuita_ws/src/debug_launch/launch/controller_controll.launch.py
@@ -3,12 +3,32 @@ import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
+import os
+import platform
+
+import re
+
+def detect_os_version():
+  os_version: str = platform.version()
+  version: str = re.search(r'~[\d]*', os_version).group()
+  version: str = version[1:]
+  version: int = int(version)
+  # print(version)
+  return version
+
+
 def generate_launch_description():
-  return launch.LaunchDescription([
-    launch_ros.actions.Node(
-      package='controller_pkg', executable='controller_publisher', output='screen'
-    ),
-    launch_ros.actions.Node(
-      package='controller_pkg', executable='controller_subscriber', output='screen'
-    )
-  ])
+  os_version: int = detect_os_version()
+  if os_version == 18:
+    return launch.LaunchDescription([
+      launch_ros.actions.Node(package='controller_pkg', node_executable='controller_publisher'),
+      launch_ros.actions.Node(package='motor_pkg',      node_executable='motor_subscriber')
+    ])
+  elif os_version == 22:
+    return launch.LaunchDescription([
+      launch_ros.actions.Node(package='controller_pkg', executable='controller_publisher'),
+      launch_ros.actions.Node(package='motor_pkg',      executable='motor_subscriber')
+    ])
+
+if __name__ == '__main__':
+  detect_os_version()
