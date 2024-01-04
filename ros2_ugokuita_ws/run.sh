a4d74095e2be2a00841411a5b2d91a8e0e8d2b3d
kanada <acaValkyrie@gmail.com>
2024-01-04 18:47:57 +0900

    ydlidar用のlaunchがJetson上で動くように変更

--- ros2_ugokuita_ws/run.sh
+++ ros2_ugokuita_ws/run.sh
@@ -1,3 +1,3 @@
-colcon build --symlink --packages-select controller_pkg motor_pkg  debug_launch
+colcon build --symlink --packages-select controller_pkg motor_pkg  debug_launch ydlidar_ros2_driver
 . install/setup.bash
 ros2 launch debug_launch controller_controll.launch.py
