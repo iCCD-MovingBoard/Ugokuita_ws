9bae2b58664d6e6f3c74241937f8bc88026945de
acaValkyrie <acavalkyrie@gmail.com>
2024-01-11 11:27:34 +0900

    ydlidar sdkを再度追加し直し

--- YDLidar-SDK/build/python/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iccd/Documents/Ugokuita_ws/YDLidar-SDK/python
-# Build directory: /home/iccd/Documents/Ugokuita_ws/YDLidar-SDK/build/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ydlidar_py_test "/usr/bin/python" "/home/iccd/Documents/Ugokuita_ws/YDLidar-SDK/python/test/pytest.py")
-set_tests_properties(ydlidar_py_test PROPERTIES  ENVIRONMENT "PYTHONPATH=/opt/ros/dashing/lib/python3.6/site-packages:/home/iccd/Documents/Ugokuita_ws/YDLidar-SDK/build/python")
-subdirs("examples")
--- YDLidar-SDK/build/python/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iccd/Documents/Ugokuita_ws/YDLidar-SDK/python/examples
-# Build directory: /home/iccd/Documents/Ugokuita_ws/YDLidar-SDK/build/python/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
