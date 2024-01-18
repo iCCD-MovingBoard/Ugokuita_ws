0925ade8bda5412bcddfc1d19c6dec393aa608c6
kanada <acaValkyrie@gmail.com>
2024-01-18 19:27:57 +0900

    ./run.sh buildとしたときだけcolcon buildするように変更

--- ros2_ugokuita_ws/run.sh
+++ ros2_ugokuita_ws/run.sh
@@ -1,3 +1,11 @@
-colcon build --symlink --packages-select controller_pkg motor_pkg  debug_launch ydlidar_ros2_driver
+#!/bin/bash
+if [ $# -eq 1 ]; then
+  if [ $1 = "build" ]; then
+    #echo "build"
+    colcon build --symlink --packages-select controller_pkg motor_pkg  debug_launch ydlidar_ros2_driver
+  fi
+fi
+#echo "run"
 . install/setup.bash
 ros2 launch debug_launch controller_controll.launch.py
+# ros2 launch ydlidar_ros2_driver ydlidar_launch_view.py
