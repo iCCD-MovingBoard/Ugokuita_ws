d407c9526204d32be0521a0732de7726823638d7
acaValkyrie <acavalkyrie@gmail.com>
2024-04-25 17:55:04 +0900

    add debug_display library

---
+++ ros2_ugokuita_ws/src/common/debug_display.py
@@ -0,0 +1,24 @@
+import serial
+import time
+
+class DebugDisplay:
+    def __init__(self, device_name='/dev/debug_display'):
+        self.port = serial.Serial(device_name, '115200', timeout=0.1)
+    def write(self, message):
+        self.port.write(bytes(f'{message}', encoding='ascii'))
+    def close(self):
+        self.port.close()
+
+def main():
+    debug_display = DebugDisplay()
+    for i in range(100):
+        message = ''
+        for j in range(i):
+            message += f'{i}'
+        message+='\n'
+        debug_display.write( message )
+        print( message , end='')
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    main()
