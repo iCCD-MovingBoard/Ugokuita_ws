4c006732d755df0bf044ba852d19f9e2bc7c6016
csenet <admin@k1h.dev>
2024-05-06 21:26:33 +0900

    update to old version

--- setup_realsense.sh
+++ setup_realsense.sh
@@ -1,22 +1,3 @@
-# Realsense Setup
-
-sudo mkdir -p /etc/apt/keyrings
-curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
-sudo tee /etc/apt/sources.list.d/librealsense.list
-sudo apt-get update
-sudo apt-get install librealsense2-dkms
-sudo apt-get install librealsense2-utils
-sudo apt-get install librealsense2-dev
-sudo apt-get install librealsense2-dbg
-
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl # if you haven't already installed curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt install ros-$ROS_DISTRO-realsense2-*
-sudo apt-get install python3-rosdep -y
-sudo rosdep init --include-eol-distros # for Foxy and earlier
-sudo rosdep update --include-eol-distros # for Foxy and earlier
-rosdep install -i --from-path src --rosdistro $ROS_DISTRO --skip-keys=librealsense2 -y
-
-sudo apt-get install ros-dashing-diagnostic-updater -y
+sudo apt-get install ros-dashing-cv-bridge ros-dashing-librealsense2 ros-dashing-message-filters ros-dashing-image-transport -y
+sudo apt-get install -y libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev
+sudo apt-get install -y libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev
