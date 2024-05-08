defd723e8027ee41a04e69947bfb0cdc54fac112
csenet <admin@k1h.dev>
2024-05-08 21:57:47 +0900

    前方の障害物を検知する機能を実装

--- ros2_ugokuita_ws/src/collision_avoid_pkg/setup.py
+++ ros2_ugokuita_ws/src/collision_avoid_pkg/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'realsense_subscriver = collision_avoid_pkg.subscriver:main',
         ],
     },
 )
