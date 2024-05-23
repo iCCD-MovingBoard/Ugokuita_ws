b74f5b587132f489be26c00f579128cfaf4bb765
acaValkyrie <acavalkyrie@gmail.com>
2024-05-23 20:22:06 +0900

    serial subscriberは受け取ったデータをSendCommand型からそれぞれのコマンドに変換してからUARTで出力するように変更

--- ros2_ugokuita_ws/src/serial_pkg/serial_pkg/serial_subscriber.py
+++ ros2_ugokuita_ws/src/serial_pkg/serial_pkg/serial_subscriber.py
@@ -4,58 +4,37 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from .lib import str_converter
 from .lib import uart
+from ...common.type_difinition import SendCommand, RecieveCommand
 
 class SerialSubscriber(Node):
   def __init__(self):
     super().__init__('serial_subscriber')
-    self.subscription = self.create_subscription(String, 'serial_topic', self.listener_callback, 10)
+    self.subscription = self.create_subscription(SendCommand, 'serial_send_topic', self.listener_callback, 10)
     self.subscription  # prevent unused variable warning
-    self.canMove = True
+    self.publisher = self.create_publisher(RecieveCommand, 'serial_recieve_topic', 10)
 
   def listener_callback(self, msg):
     self.get_logger().info('I heard: "%s"' % msg.data)
-    if msg.data == '#stop':
-      if self.canMove == True:
-        self.get_logger().warn('STOP')
-      self.canMove = False
-      return # msg.dataにコントローラーの入力は含まれていないので、ここで処理を終了する
     
-    if msg.data == '#start':
-      if self.canMove == False:
-        self.get_logger().warn('START')
-      self.canMove = True
-      return # msg.dataにコントローラーの入力は含まれていないので、ここで処理を終了する
-    
-    controller_inputs: dict = str_converter.to_dict(msg.data)
-    
-    axis_x = int(controller_inputs['L_Axis_x'])
-    axis_y = int(controller_inputs['L_Axis_y'])
-    right = -axis_x - axis_y
-    left  =  axis_x - axis_y
-    
-    if self.canMove == False:
-      if right > 0: right = 0
-      if left > 0: left = 0
-      uart.send_to_motordriver('B300')
-    
-    scaled_speed_r = uart.scale_speed(right)
-    scaled_speed_l = uart.scale_speed(left)
-    adjusted_speed_r, adjusted_speed_l = uart.adjust_speed(scaled_speed_r, scaled_speed_l)
-    
-    uart.send_to_motordriver(f'R{adjusted_speed_r}')
-    uart.send_to_motordriver(f'L{adjusted_speed_l}')
-    
+    right = msg.r
+    if right: uart.send_to_motordriver(f'R{right}')
+    left = msg.l
+    if left: uart.send_to_motordriver(f'L{left}')
+    h = msg.h
+    if h: uart.send_to_motordriver(f'H{h}')
+    b = msg.b
+    if b: uart.send_to_motordriver(f'B{b}')
 
 def main(args=None):
   try:
     rclpy.init(args=args)
-    controller_subscriber = MotorSubscriber()
-    rclpy.spin(controller_subscriber)
+    serial_subscriber = SerialSubscriber()
+    rclpy.spin(serial_subscriber)
   except KeyboardInterrupt:
     pass
   finally:
     uart.send_to_motordriver(0, 0, 0, 0)
-    controller_subscriber.destroy_node()
+    serial_subscriber.destroy_node()
     uart.uart_port.close()
     rclpy.shutdown()
 
