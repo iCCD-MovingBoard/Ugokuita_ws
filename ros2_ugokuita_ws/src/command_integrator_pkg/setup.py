cc35e833bcfd5ae49c5ab61d91a4f605bff2b479
acaValkyrie <acavalkyrie@gmail.com>
2024-05-24 18:05:29 +0900

    Refactor setup.cfg to use common_pkg directory

--- ros2_ugokuita_ws/src/command_integrator_pkg/setup.py
+++ ros2_ugokuita_ws/src/command_integrator_pkg/setup.py
@@ -6,13 +6,13 @@ submodules = 'command_integrator_pkg/lib'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name, submodules],
+    packages=[package_name, submodules, 'common_pkg'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'common'],
+    install_requires=['setuptools', 'common_pkg'],
     zip_safe=True,
     maintainer='canada',
     maintainer_email='acavalkyrie@gmail.com',
