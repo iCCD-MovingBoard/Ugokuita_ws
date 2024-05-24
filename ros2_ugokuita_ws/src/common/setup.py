473565759281cc000b87a5fafc1d13690fc4152c
acaValkyrie <acavalkyrie@gmail.com>
2024-05-24 13:08:40 +0900

    commonをパッケージにした

---
+++ ros2_ugokuita_ws/src/common/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'common'
+# submodules = 'common/lib'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name]#, submodules],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='canada',
+    maintainer_email='acavalkyrie@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
