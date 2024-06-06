a27b6ef6334238436af4277a6ee66965244284ec
acaValkyrie <acavalkyrie@gmail.com>
2024-06-06 22:08:04 +0900

    subscriptionの定義ミスの修正

--- ros2_ugokuita_ws/src/command_integrator_pkg/command_integrator_pkg/command_integrator_subscriber.py
+++ ros2_ugokuita_ws/src/command_integrator_pkg/command_integrator_pkg/command_integrator_subscriber.py
@@ -1,13 +1,11 @@
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
-LIDAR_ID = 1
 
 class CommandIntegrator(Node):
     def __init__(self):
         super().__init__('command_integrator_negotiator')
-        self.subscription = self.create_subscription(String, 'request_topic', 10)
+        self.subscription = self.create_subscription(String, 'request_topic', self.listener_callback, 10)
         # self.subscription  # prevent unused variable warning
         self.publisher = self.create_publisher(String, 'serial_send_topic', 10)
 
