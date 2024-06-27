2d5ec0a8ca8d9731d79f2cf4b378e02710acdcd8
acaValkyrie <acavalkyrie@gmail.com>
2024-06-27 19:40:30 +0900

    serialのreadの頻度を下げた

--- ros2_ugokuita_ws/src/serial_pkg/serial_pkg/serial_publisher.py
+++ ros2_ugokuita_ws/src/serial_pkg/serial_pkg/serial_publisher.py
@@ -7,7 +7,7 @@ class SerialPublisher(Node):
     def __init__(self):
         super().__init__('serial_publisher')
         self.publisher_ = self.create_publisher(String, 'serial_recieve_topic', 10)
-        timer_period = 0.0001  # seconds
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
